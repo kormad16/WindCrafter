46bf6b4f3210be16315272070759e3bc3596a944
Hacktix <zecmad16@htlkaindorf.at>
2019-11-24 22:33:45 +0100

    Added Hero's Charm Villager Trade

---
+++ src/main/java/at/kaindorf/windcrafter/entities/villager/ArmorerEquipmentTrades.java
@@ -0,0 +1,19 @@
+package at.kaindorf.windcrafter.entities.villager;
+
+import at.kaindorf.windcrafter.init.ItemManager;
+import net.minecraft.entity.IMerchant;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.village.MerchantRecipe;
+import net.minecraft.village.MerchantRecipeList;
+
+import java.util.Random;
+
+public class ArmorerEquipmentTrades implements EntityVillager.ITradeList {
+
+    @Override
+    public void addMerchantRecipe(IMerchant merchant, MerchantRecipeList recipeList, Random random) {
+        recipeList.add(new MerchantRecipe(new ItemStack(ItemManager.JOY_PENDANT, 20), new ItemStack(ItemManager.HEROS_CHARM, 1)));
+    }
+}
